81feae88d01896ac56b08ecdf9ba76d49916610b
darshangoswami <goswamidarshan999@gmail.com>
2024-07-28 21:24:51 -0500

    update frontend to play with shuffle

--- frontend/src/components/ShuffledPlaylist.jsx
+++ frontend/src/components/ShuffledPlaylist.jsx
@@ -1,11 +1,33 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
 axios.defaults.withCredentials = true;
 
 function ShuffledPlaylist() {
+  const [isShuffling, setIsShuffling] = useState(false);
   const { playlistId } = useParams();
+
+  const playWithShuffle = async () => {
+    setIsShuffling(true);
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:5000/play-with-shuffle/${playlistId}`,
+        { withCredentials: true }
+      );
+      alert(response.data.message);
+      console.log(response.data);
+    } catch (error) {
+      if (error.response) {
+        alert(`Error: ${error.response.data.error}`);
+      } else {
+        alert("An error occurred while shuffling the queue");
+      }
+      console.error("Error shuffling queue:", error);
+    } finally {
+      setIsShuffling(false);
+    }
+  };
 
   const createShuffledPlaylist = async () => {
     try {
@@ -23,8 +45,19 @@ function ShuffledPlaylist() {
   return (
     <div>
       <h1>Playlist</h1>
-      <button>Play with Shufflefy</button>
+      <button onClick={playWithShuffle} disabled={isShuffling}>
+        {isShuffling ? "Shuffling..." : "Play With Shufflefy"}
+      </button>
       <button onClick={createShuffledPlaylist}>Create Shuffled Playlist</button>
+      <ul>
+        <h3>Before Playing with Shufflefy:</h3>
+        <li>
+          <p>1. Make sure Spotify is active on the device you want to play.</p>
+        </li>
+        <li>
+          <p>2. Clear your queue before shuffling.</p>
+        </li>
+      </ul>
     </div>
   );
 }
